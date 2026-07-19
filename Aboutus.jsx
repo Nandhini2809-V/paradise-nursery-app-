08fc16414197e83a9263e8591b968ad351f01de4
Nandhini2809-V <nandhini28092006@gmail.com>
2026-07-19 21:17:54 +0530

    Create Aboutus.jsx

---
+++ Aboutus.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div style={{padding: '20px', textAlign: 'center'}}>
+      <h1>About Paradise Nursery</h1>
+      <p>We are passionate about bringing nature into your home.</p>
+      <p>Founded in 2020, Paradise Nursery offers a wide variety of indoor and outdoor plants. 
+      Our mission is to make plant shopping easy and bring greenery to every home.</p>
+      <img src="https://images.unsplash.com/photo-1585320806297-9794b3e4eeae?w=500" alt="Nursery" width="300"/>
+    </div>
+  );
+}
+export default AboutUs;
